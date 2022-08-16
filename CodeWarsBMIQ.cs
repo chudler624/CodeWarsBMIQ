022fec665d2dd1e907d20999f131c0aca906cac8
Casey Hudler <chudler624@gmail.com>
2022-08-16 19:00:36 -0400

    initial add

---
+++ CodeWarsBMIQ.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Methods
+    {
+        public static string Bmi(double weight, double height)
+        {
+                
+            double userBMI = (weight / (height * height));
+
+            if (userBMI <= 18.5)
+            return Console.WriteLine("Underweight"); 
+
+            if (userBMI <= 25.0)
+             return Console.WriteLine("Normal");
+
+            if (userBMI <= 30.0)
+            return Console.WriteLine("Overweight");
+
+            else (userBMI > 30.0)
+            return Console.WriteLine("Obese"); 
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
